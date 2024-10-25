b822542968897dda5cb167ac555e06d888a564ea
Sami-1166 <sami.613619@gmail.com>
2024-10-25 14:34:39 +0500

    Add files via upload
    Lab Mid Question 1

---
+++ LabMidQ1/LabMidQ1/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+
+class DFA
+{
+    private enum State
+    {
+        Start,
+        FirstChar,
+        SubsequentChar,
+        Accept,
+        Reject
+    }
+
+    public static bool IsValidVariableName(string input)
+    {
+        State currentState = State.Start;
+
+        foreach (char c in input)
+        {
+            switch (currentState)
+            {
+                case State.Start:
+                    if (char.IsLetter(c) || c == '_')
+                    {
+                        currentState = State.FirstChar;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    break;
+
+                case State.FirstChar:
+                    if (char.IsLetterOrDigit(c) || c == '_')
+                    {
+                        currentState = State.SubsequentChar;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    break;
+
+                case State.SubsequentChar:
+                    if (char.IsLetterOrDigit(c) || c == '_')
+                    {
+                        currentState = State.Accept;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    break;
+
+                case State.Accept:
+                    if (char.IsLetterOrDigit(c) || c == '_')
+                    {
+                        // Stay in accept state
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                    break;
+
+                default:
+                    return false;
+            }
+        }
+
+        return currentState == State.Accept || currentState == State.SubsequentChar;
+    }
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a variable name to check:");
+        string input = Console.ReadLine();
+
+        if (IsValidVariableName(input))
+        {
+            Console.WriteLine($"{input} is a valid C variable name.");
+        }
+        else
+        {
+            Console.WriteLine($"{input} is not a valid C variable name.");
+        }
+    }
+}
